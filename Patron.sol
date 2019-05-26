5e71920ae8339ad174e9b507d01dc54c1cc6e44e
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-25 22:50:57 -0400

    Ownable contract added

--- Patron.sol
+++ Patron.sol
@@ -6,3 +6,28 @@
  */
 
 pragma solidity ^0.5.0;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * modifier, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address payable internal owner;
+    
+    /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    constructor() internal {
+        owner = msg.sender;
+    }
+    
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner {
+        require(msg.sender == owner, "Unautorized access!");
+        _;
+    }
+}
