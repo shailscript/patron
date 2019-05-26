3c533f65994b7fd882335d9b8947b6b689381b22
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-25 22:52:01 -0400

    Destructible contract added

--- Patron.sol
+++ Patron.sol
@@ -31,3 +31,24 @@ contract Ownable {
         _;
     }
 }
+
+
+/**
+ * @title Destructible
+ * @dev Base contract that can be destroyed by owner. All funds in contract will be sent to the owner.
+ */
+contract Destructible is Ownable {
+ 
+  constructor() internal payable { } 
+ 
+  /**
+   * @dev Transfers the current balance to the owner and terminates the contract. 
+   */
+  function destroy() public onlyOwner {
+    selfdestruct(owner);
+  }
+ 
+  function destroyAndSend(address payable _recipient) public onlyOwner {
+    selfdestruct(_recipient);
+  }
+}
